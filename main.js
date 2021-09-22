58fc5dac3da6445ebc808ae865e01c7915f6188d
JonathanXDR <jonathan.russ@bluewin.ch>
2021-09-22 19:22:45 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let list = document.getElementById("toDo");
+
+function createElement() {
+  if (document.getElementById("input").value === null) {
+    alert("Taskname not specified!");
+  } else {
+    let element = document.createElement("toDo");
+  }
+}
+
+// console.log(input);
