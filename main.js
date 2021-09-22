8586c4b656121a21dc10292761163986711e5f23
JonathanXDR <jonathan.russ@bluewin.ch>
2021-09-22 19:29:32 +0200

    test

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let list = document.getElementById("toDo");
 
+let taskName = document.getElementById("input").value
+
+console.log(taskName)
+
+setInterval(createElement, 100);
 function createElement() {
-  if (document.getElementById("input").value === null) {
+  if (taskName === "") {
     alert("Taskname not specified!");
   } else {
     let element = document.createElement("toDo");
   }
 }
-
-// console.log(input);
